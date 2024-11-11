509ab370dc8ac11d81eda55584063fa9975a165d
Alma-yovell <alma.yovell@gmail.com>
2024-11-11 15:38:35 +0200

    megnify img

--- src/componentsJs/DiagramStep2.js
+++ src/componentsJs/DiagramStep2.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../componentsCss/DiagramStep2.css';
 import StepsBtnDiagram from "./StepsBtnDiagram";
 import { useLocation } from 'react-router-dom';
-
 
 function DiagramStep2() {
     const location = useLocation();
@@ -14,10 +13,16 @@ function DiagramStep2() {
 
     const color = selectedOption === 'נפה' ? '#1cb4e3' : '#ff9900';
 
+    // State to track if image is magnified
+    const [isMagnified, setIsMagnified] = useState(false);
+
+    // Function to toggle magnification and rotation
+    const magnifyPic = () => {
+        setIsMagnified(!isMagnified);
+    };
 
     return (
-
-        <div className="DiagramStep2" >
+        <div className="DiagramStep2">
             <StepsBtnDiagram currentStep={2} />
             <div className='title-DiagramStep2'>
                 מבנה ייעוד ותפקידי
@@ -33,14 +38,15 @@ function DiagramStep2() {
 
             <img
                 src={imagePath}
-                className="stpe2Img"
+                className={`stpe2Img ${isMagnified ? 'magnified' : ''}`}
                 alt="stpe2Img"
+                onClick={magnifyPic}
             />
+            <div className={`shadow ${isMagnified ? 'shadow-active' : ''}`}></div>
+            {/* <img src={process.env.PUBLIC_URL + '/assests/imgs/closeBtn.png'} className="closeBtn-img" alt="closeBtn" ></img> */}
             <div className='text-img'>אפשר להגדיל בלחיצה</div>
 
-            <img src={process.env.PUBLIC_URL + '/assests/imgs/blackArrow.png'} className="blackArrow" alt="blackArrow"></img>
-
-
+            {/* <img src={process.env.PUBLIC_URL + '/assests/imgs/blackArrow.png'} className="blackArrow" alt="blackArrow" ></img> */}
         </div>
     );
 }
